77860fe39cb40bd44fa53e13aaa50a8a56c18930
bhavesh-suvalaka <bhaveshkumars@sahaj.ai>
2023-09-12 23:40:09 +0530

    setting up

---
+++ src/main/java/com/example/fibers/FibersApplication.java
@@ -0,0 +1,13 @@
+package com.example.fibers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FibersApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(
+                FibersApplication.class, args);
+		Thread.ofVirtual().start(() -> System.out.println("hello world"));
+    }
+}
